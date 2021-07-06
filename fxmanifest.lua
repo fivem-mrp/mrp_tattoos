8eb29cd45b4f6df015a38affdc0070736f2f5a98
mufty <gubriansky@sernato.sk>
2021-07-06 14:28:42 +0200

    Include all DLC tattoos

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,4 +14,19 @@ server_scripts {
 	'server.lua'
 }
 
-file 'AllTattoos.json'
+files {
+  'mpheist3/shop_tattoo.meta',
+  'mpheist3/mpheist3_overlays.xml',
+  'mpvinewood/shop_tattoo.meta',
+  'mpvinewood/mpvinewood_overlays.xml',
+  'mpheist4/shop_tattoo.meta',
+  'mpheist4/mpheist4_overlays.xml',
+  'AllTattoos.json',
+}
+
+data_file 'TATTOO_SHOP_DLC_FILE' 'mpheist3/shop_tattoo.meta'
+data_file 'PED_OVERLAY_FILE' 'mpheist3/mpheist3_overlays.xml'
+data_file 'TATTOO_SHOP_DLC_FILE' 'mpvinewood/shop_tattoo.meta'
+data_file 'PED_OVERLAY_FILE' 'mpvinewood/mpvinewood_overlays.xml'
+data_file 'TATTOO_SHOP_DLC_FILE' 'mpheist4/shop_tattoo.meta'
+data_file 'PED_OVERLAY_FILE' 'mpheist4/mpheist4_overlays.xml'
